f80253cb83b34da74974f50c20c356744da66e08
Ivan Cadiang <iacadiang@up.edu.ph>
2025-02-02 22:12:58 +0800

    fix: handle CORS issue for local testing

--- mosip-sdk-api/main.py
+++ mosip-sdk-api/main.py
@@ -1,11 +1,24 @@
 from auth import AuthHandler
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from mosip_auth_sdk.models import DemographicsModel
 
 server = FastAPI()
 auth = AuthHandler()
 
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+server.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class DemographicRequestData(BaseModel):
     uin: str
