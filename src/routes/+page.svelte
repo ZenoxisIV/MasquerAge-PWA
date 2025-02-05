fb53d94f1eddbb80105dac7a3624a367c3d46c32
Ivan Cadiang <iacadiang@up.edu.ph>
2025-02-05 18:11:30 +0800

    style: add modals for each response

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import { Button, Card, Datepicker, Label, Input } from 'flowbite-svelte';
-
+	import { Button, Card, Datepicker, Label, Input, Modal } from 'flowbite-svelte';
+	import { CheckCircleSolid, CloseCircleSolid, ExclamationCircleSolid } from 'flowbite-svelte-icons';
+	
+	let verifiedPrompt: boolean = false, rejectedPrompt: boolean = false, invalidPrompt: boolean = false;
+	let modalOpen: boolean;
+	
 	let uin: string = '';
 	let dateOfBirth: Date;
-	let responseMessage: string = '';
 
 	async function validateID() {
 		let dob: string = new Date(dateOfBirth.getTime() + Math.abs(dateOfBirth.getTimezoneOffset() * 60000)).toISOString().split('T')[0].replace(/-/g, '/');
@@ -18,25 +21,23 @@
 			const data = await response.json();
 
 			if (data.authStatus) {
-				responseMessage = 'ID Validation: ' + data.authStatus
-
 				const birthYear: number = new Date(dateOfBirth).getFullYear();
 				const currentYear: number = new Date().getFullYear();
 				const age: number = currentYear - birthYear;
 
-				if (age < 18) {
-					responseMessage += ' (User is under 18)';
+				if (age >= 35) {
+					verifiedPrompt = true;	
 				} else {
-					responseMessage += ' (User is 18 or older)';
+					rejectedPrompt = true;
 				}
 			} else {
-				responseMessage = 'ID Validation: ' + data.authStatus
-				responseMessage += ' (Invalid ID)'
+				invalidPrompt = true;
 			}
 
+			modalOpen = verifiedPrompt || rejectedPrompt || invalidPrompt;
+
 		} catch (error) {
-			console.error('Error:', error);
-			responseMessage = 'An error occurred';
+			console.error(error);
 		}
 	}
 </script>
@@ -47,6 +48,22 @@
 </svelte:head>
 
 <section>
+	<Modal bind:open={modalOpen} size="xs" autoclose outsideclose>
+		<div class="text-center">
+			{#if verifiedPrompt}
+				<CheckCircleSolid class="mx-auto mb-4 text-green-600 w-24 h-24 dark:text-green-400" />
+				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">This person is 35 or older.</h3>
+			{:else if rejectedPrompt}
+				<CloseCircleSolid class="mx-auto mb-4 text-red-600 w-24 h-24 dark:text-red-400" />
+				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">This person is below 35.</h3>
+			{:else if invalidPrompt}
+				<ExclamationCircleSolid class="mx-auto mb-4 text-yellow-400 w-24 h-24 dark:text-yellow-400" />
+				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">Invalid ID. Please try again.</h3>
+			{/if}
+			<Button color="alternative">Close</Button>
+		</div>
+	</Modal>
+
 	<Card class="mx-auto mt-6 mb-6" size="sm" border={false}>
 		<form class="flex flex-col space-y-6" on:submit|preventDefault={validateID}>
 			<h3 class="text-xl font-medium text-gray-900 dark:text-white">MOSIP Anonymous Age Verification</h3>
@@ -67,8 +84,6 @@
 			New to AgeCloak? <a href="/generate-qr" class="text-primary-700 hover:underline dark:text-primary-500"> Create a MOSIP ID </a>
 		</div>
 	</Card>
-
-	<p class="t-normal text-gray-700 dark:text-gray-400 leading-tight">{responseMessage}</p>
 </section>
 
 <style>
