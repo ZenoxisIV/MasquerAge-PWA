c5dd2942080330cd146f06d299bee185340b517e
Ivan Cadiang <iacadiang@up.edu.ph>
2025-02-06 00:27:17 +0800

    feat: add age verifier via QR Scanner

--- src/routes/qr-scanner/QRData.svelte
+++ src/routes/qr-scanner/QRData.svelte
@@ -1,101 +1,85 @@
 <script lang="ts">
-	export let active: boolean;
+	import { Button, Modal } from 'flowbite-svelte';
+	import { CheckCircleSolid, CloseCircleSolid, ExclamationCircleSolid } from 'flowbite-svelte-icons';
+
+	let verifiedPrompt: boolean = false, rejectedPrompt: boolean = false, invalidPrompt: boolean = false;
+	let modalOpen: boolean;
+
 	export let decodedData: string;
 	export let onNewScan: () => void;
 
-	let regex: any;
+	async function validateID(data: string) {
+		let parsedData;
 
-	$: if (active) {
-		const urlExp =
-			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
-		regex = new RegExp(urlExp);
+		try {
+			parsedData = JSON.parse(data);
+		} catch (error) {
+			console.error(error);
+			return;
+		}
+
+		// TODO: query database for uin (map pcn to uin)
+		let pcn: string = parsedData.PCN;
+		let dateOfBirth: Date = new Date(parsedData.subject.DOB)
+		let dob: string = new Date(dateOfBirth.getTime() + Math.abs(dateOfBirth.getTimezoneOffset() * 60000)).toISOString().split('T')[0].replace(/-/g, '/');
+
+		// Note: for now, we hardcode custom values for testing
+		let uin: string = "";
+		dob = "";
+
+		try {
+			const response = await fetch('http://127.0.0.1:3000/dob', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ uin, dob })
+			});
+			const data = await response.json();
+
+			if (data.authStatus) {
+				const birthYear: number = new Date(dateOfBirth).getFullYear();
+				const currentYear: number = new Date().getFullYear();
+				const age: number = currentYear - birthYear;
+
+				if (age >= 35) {
+					verifiedPrompt = true;	
+				} else {
+					rejectedPrompt = true;
+				}
+			} else {
+				invalidPrompt = true;
+			}
+
+			modalOpen = verifiedPrompt || rejectedPrompt || invalidPrompt;
+
+		} catch (error) {
+			console.error(error);
+		}
 	}
 </script>
 
 <slot {decodedData}>
-	<div class="results">
-		<div class="results__data">
-			{#if !decodedData || decodedData === ''}
-				<span class="results__empty">No data found. </span>
-			{:else if decodedData?.match(regex)}
-				<a class="results__link" href={decodedData} target="_blank" rel="noopener noreferrer">
-					{decodedData}
-				</a>
-			{:else if decodedData}
-				{decodedData};
+	{#if decodedData}
+		{validateID(decodedData)}
+	{/if}
+	<Modal bind:open={modalOpen} size="xs" autoclose outsideclose>
+		<div class="text-center">
+			{#if verifiedPrompt}
+				<CheckCircleSolid class="mx-auto mb-4 text-green-600 w-24 h-24 dark:text-green-400" />
+				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">This person is 35 or older.</h3>
+			{:else if rejectedPrompt}
+				<CloseCircleSolid class="mx-auto mb-4 text-red-600 w-24 h-24 dark:text-red-400" />
+				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">This person is below 35.</h3>
+			{:else if invalidPrompt}
+				<ExclamationCircleSolid class="mx-auto mb-4 text-yellow-400 w-24 h-24 dark:text-yellow-400" />
+				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">Invalid ID. Please try again.</h3>
 			{/if}
+			<Button on:click={onNewScan}>New Scan</Button>
 		</div>
-		<div class="results__button-container">
-			<button type="button" class="results__button" on:click={onNewScan}>
-				<span>New Scan</span>
-			</button>
-		</div>
-	</div>
+	</Modal>
 </slot>
 
 <style>
-	.results {
-		width: 100%;
-		max-width: 500px;
-	}
 
-	.results__data {
-		margin: 50px 0 80px 0;
-		padding: 20px 20px 27px 20px;
-
-		word-break: break-all;
-
-		border: 2px dashed #3d3d3d;
-		border-radius: 20px;
-
-		font-size: 1.3rem;
-	}
-
-	.results__empty {
-		color: #959595;
-
-		font-style: italic;
-	}
-
-	.results__button-container {
-		display: flex;
-		justify-content: center;
-	}
-
-	.results__button {
-		display: flex;
-
-		align-items: center;
-
-		padding: 14px 16px;
-
-		cursor: pointer;
-		transition-duration: 300ms;
-		transition-property: background-color, transform;
-
-		color: white;
-
-		border: 0;
-		border-radius: 8px;
-		background-color: #23cd00;
-
-		font-size: 1.5rem;
-		font-weight: 500;
-	}
-
-	.results__button:hover {
-		background-color: #39b300;
-	}
-
-	.results__link,
-	.results__link:visited {
-		transition: color 200ms;
-
-		color: #7094ff;
-	}
-
-	.results__link:hover,
-	.results__link:active {
-		color: #486cd9;
-	}
 </style>
