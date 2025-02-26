e23cc5a766293244531b9849a5a6af2658f7e244
Ivan Cadiang <iacadiang@up.edu.ph>
2025-02-26 17:27:27 +0800

    db: update fields with stricter compliance

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,15 +1,18 @@
-import { mysqlTable, serial, text, int, date } from 'drizzle-orm/mysql-core';
+import { mysqlTable } from 'drizzle-orm/mysql-core';
+import * as t from "drizzle-orm/mysql-core";
 
 export const usersTable = mysqlTable('users', {
-	id: int("id").primaryKey().autoincrement(),
-	pcn: text("pcn"),
-	uin: text("uin"),
-	firstName: text("firstName"),
-	middleName: text("middleName"),
-	lastName: text("lastName"),
-	suffix: text("suffix"),
-	sex: text("sex"),
-	dateOfBirth: date(),
-	placeOfBirth: text("placeOfBirth"),
-	photo: text("photo")
+	id: t.int("id").primaryKey().autoincrement(),
+	pcn: t.varchar("pcn", { length: 19 }).notNull(),
+	uin: t.varchar("uin", { length: 10 }).notNull(),
+	firstName: t.varchar("firstName", { length: 256 }).notNull(),
+	middleName: t.varchar("middleName", { length: 256 }),
+	lastName: t.varchar("lastName", { length: 256 }).notNull(),
+	suffix: t.varchar("suffix", { length: 5 }),
+	sex: t.mysqlEnum(["Male", "Female"]).notNull(),
+	maritalStatus: t.mysqlEnum(["Single", "Married", "Divorced", "Separated", "Widowed"]).notNull(),
+	bloodType: t.mysqlEnum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]).notNull(),
+	dateOfBirth: t.date().notNull(),
+	placeOfBirth: t.varchar("placeOfBirth", { length: 256 }).notNull(),
+	photo: t.text("photo")
 });
