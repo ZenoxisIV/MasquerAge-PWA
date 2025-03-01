e59d233fd5c4be24674452db3b0cb38c90d8fb4d
Ivan Cadiang <iacadiang@up.edu.ph>
2025-03-02 01:32:26 +0800

    style: make UI prompt elements larger

--- src/routes/qr-scanner/QRData.svelte
+++ src/routes/qr-scanner/QRData.svelte
@@ -52,26 +52,27 @@
 				<div class="flex flex-col items-center">
 					<div class="relative">
 						{#if result.photo}
-							<img src="{`data:image/png;base64,${result.photo}`}" alt="Profile" class="w-24 h-24 rounded-full border-4 border-green-600 dark:border-green-400" />
+							<img src="{`data:image/png;base64,${result.photo}`}" alt="Profile" class="w-60 h-60 rounded-lg border-4 border-green-600 dark:border-green-400" />
 						{:else}
-							<div class="w-24 h-24 rounded-full border-4 border-gray-300 dark:border-gray-500 flex items-center justify-center text-gray-500 text-sm">
+							<div class="w-24 h-24 rounded-lg border-4 border-gray-300 dark:border-gray-500 flex items-center justify-center text-gray-500 text-sm">
 								No Image
 							</div>
 						{/if}
-						<CheckCircleSolid class="absolute bottom-0 right-0 w-8 h-8 text-green-600 dark:text-green-400 stroke-white stroke-2" />
 					</div>
-					<h3 class="mt-4 mb-5 text-lg font-normal text-black dark:text-gray-400">
-						This person is 35 or older.
-					</h3>
+					<div class="mt-4 mb-5 flex items-center gap-2 text-lg font-normal text-black dark:text-gray-400">
+						<CheckCircleSolid class="w-6 h-6 text-green-600 dark:text-green-400 stroke-white stroke-2" />
+						<span class="text-xl">This person is 35 or older.</span>
+					</div>
 				</div>
+
 			{:else if rejectedPrompt}
-				<CloseCircleSolid class="mx-auto mb-4 text-red-600 w-24 h-24 dark:text-red-400" />
-				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">
+				<CloseCircleSolid class="mx-auto mb-4 text-red-600 w-36 h-36 dark:text-red-400" />
+				<h3 class="mb-5 text-xl font-normal text-black dark:text-gray-400">
 					This person is below 35.
 				</h3>
 			{:else if invalidPrompt}
-				<ExclamationCircleSolid class="mx-auto mb-4 text-yellow-400 w-24 h-24 dark:text-yellow-400" />
-				<h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">
+				<ExclamationCircleSolid class="mx-auto mb-4 text-yellow-400 w-36 h-36 dark:text-yellow-400" />
+				<h3 class="mb-5 text-xl font-normal text-black dark:text-gray-400">
 					Invalid ID. Please try again.
 				</h3>
 			{/if}
