086d207ccaa37abd418a2a9e9a50e23b301a4344
ZenoxisIV <iacadiang@up.edu.ph>
2025-04-02 21:37:13 +0800

    refactor(QRScanner): save logs to a text file

--- src/routes/api/scan/+server.ts
+++ src/routes/api/scan/+server.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { json, type RequestHandler } from "@sveltejs/kit";
 import { eq } from "drizzle-orm";
 import { db } from "$lib/server/db/index";
@@ -5,17 +7,19 @@ import { usersTable, userDemographicsTable } from "$lib/server/db/schema";
 import pino from "pino";
 import { FASTAPI_URL } from '$env/static/private';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const logFilePath = path.join(__dirname, 'backend_server.log');
+
 const logger: pino.Logger = pino({
-	level: import.meta.env.MODE === "production" ? "info" : "debug",
-	transport: import.meta.env.MODE === "development" ? 
-		{ 
-			target: "pino-pretty", 
-			options: { 
-				colorize: true,
-				levelFirst: true,
-				translateTime: true
-			}
-		} : undefined,
+    level: import.meta.env.MODE === "production" ? "info" : "debug",
+    transport: {
+        target: 'pino/file',
+        options: {
+            destination: logFilePath,
+            mkdir: true
+        }
+    }
 });
 
 function formatPCN(input: string): string {
@@ -45,7 +49,11 @@ export const POST: RequestHandler = async ({ request }) => {
         try {
             parsedData = JSON.parse(data);
         } catch (error: unknown) {
-            logger.warn("QR Code parsing failed");
+            logger.error({
+                internalCode: "[Node-QRFail]",
+                status: 400,
+                err: "QR Code parsing failed"
+            });
             return json({ error: "Invalid ID" }, { status: 400 });
         }
 
@@ -57,14 +65,29 @@ export const POST: RequestHandler = async ({ request }) => {
             if (typeof parsedData === 'number') { // Front QR Processing
                 let pcn = formatPCN(parsedData.toString());
 
+                logger.info({
+                    internalCode: "[Neon-INIT]",
+                    msg: "Starting query to Neon..."
+                });
+
                 const queryResult = await db
                     .select({ uin: usersTable.uin, dateOfBirth: userDemographicsTable.dateOfBirth, photo: usersTable.photo })
                     .from(usersTable)
                     .innerJoin(userDemographicsTable, eq(usersTable.pcn, userDemographicsTable.pcn))
                     .where(eq(usersTable.pcn, pcn));
 
+                logger.info({
+                    internalCode: "[Neon-FIN]",
+                    msg: "Done querying to Neon..."
+                });
+
                 if (queryResult.length === 0) {
-                    logger.warn(`Invalid PCN lookup: ${pcn}`);
+                    logger.warn({
+                        internalCode: "[Neon-InvPCN]",
+                        status: 400,
+                        msg: `Invalid PCN lookup: ${pcn}`
+                    });
+
                     return json({ error: "Invalid ID" }, { status: 400 });
                 }
 
@@ -72,61 +95,92 @@ export const POST: RequestHandler = async ({ request }) => {
                 dobDB = queryResult[0].dateOfBirth;
                 photo = queryResult[0].photo;
 
-            } else { // Back QR Processing
-                const pcn = formatPCN(parsedData.pcn);
+                } else { // Back QR Processing
+                    const pcn = formatPCN(parsedData.pcn);
 
-                if (!validateDOB(parsedData.bd)) {
-                    return json({ error: "Invalid ID" }, { status: 400 });
+                    if (!validateDOB(parsedData.bd)) {
+                        return json({ error: "Invalid ID" }, { status: 400 });
+                    }
+
+                    logger.info({
+                        internalCode: "[Neon-INIT]",
+                        msg: "Starting query to Neon..."
+                    });
+
+                    const queryResult = await db
+                        .select({ uin: usersTable.uin })
+                        .from(usersTable)
+                        .where(eq(usersTable.pcn, pcn));
+
+                    logger.info({
+                        internalCode: "[Neon-FIN]",
+                        msg: "Done querying to Neon..."
+                    });
+
+                    if (queryResult.length === 0) {
+                        logger.warn({
+                            internalCode: "[Neon-InvPCN]",
+                            status: 400,
+                            msg: `Invalid PCN lookup: ${pcn}`
+                        });
+
+                        return json({ error: "Invalid ID" }, { status: 400 });
+                    }
+
+                    uin = queryResult[0].uin;
+                    dobDB = parsedData.bd;
+                    photo = parsedData.p;
                 }
-
-                const queryResult = await db
-                    .select({ uin: usersTable.uin })
-                    .from(usersTable)
-                    .where(eq(usersTable.pcn, pcn));
-
-                if (queryResult.length === 0) {
-                    logger.warn(`Invalid PCN lookup: ${pcn}`);
-                    return json({ error: "Invalid ID" }, { status: 400 });
-                }
-
-                uin = queryResult[0].uin;
-                dobDB = parsedData.bd;
-                photo = parsedData.p;
-            }
         } catch (error: unknown) {
-            logger.error({ error }, "Database query failed");
+            logger.error({
+                internalCode: "[Neon-DBFail]",
+                status: 500,
+                err: error
+            });
             return json({ error: "Invalid ID" }, { status: 400 });
         }
 
-		const dobMOSIP: string = dobDB.replace(/-/g, "/");
+        const dobMOSIP: string = dobDB.replace(/-/g, "/");
 
         // Verify DOB via FastAPI
         let result: { authStatus: boolean, responseTime: string, errorMessages: string};
-		try {
-			const response = await fetchWithTimeout(FASTAPI_URL, {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ uin, dob: dobMOSIP })
-			}, 5000);
+        try {
+            logger.info({
+                internalCode: "[FastAPI-INIT]",
+                msg: "Sending request to FastAPI..."
+            });
 
-			if (!response.ok) {
-				logger.warn(`FastAPI returned ${response.status} - ${response.statusText}`);
-				return json({ error: "Invalid ID" }, { status: 400 });
-			}
+            const response = await fetchWithTimeout(FASTAPI_URL, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ uin, dob: dobMOSIP })
+            }, 5000);
 
-			result = await response.json();
-            logger.info(`Response received at: ${result.responseTime}`);
-		} catch (error: unknown) {
-            logger.warn("FastAPI request failed or timed out");
-            logger.error(error);
-			return json({ error: "Invalid ID" }, { status: 400 });
-		}
+            if (!response.ok) {
+                logger.info({
+                    internalCode: "[FastAPI-SDown]",
+                    status: response.status,
+                    msg: response.statusText
+                });
+                return json({ error: "Invalid ID" }, { status: 400 });
+            }
+
+            result = await response.json();
+        } catch (error: unknown) {
+            logger.error({
+                internalCode: "[FastAPI-RTO]",
+                status: 408,
+                err: error
+            });
+            return json({ error: "Invalid ID" }, { status: 400 });
+        }
 
         if (!result.authStatus) {
-            logger.warn(`Authentication failed for UIN: ${uin}`);
-            if (result.errorMessages !== "[]") {
-                logger.error(result.errorMessages);
-            }
+            logger.error({
+                internalCode: "[FastAPI-AuthFail]",
+                status: 401,
+                err: result.errorMessages
+            });
             return json({ error: "Invalid ID" }, { status: 400 });
         }
 
@@ -138,7 +192,11 @@ export const POST: RequestHandler = async ({ request }) => {
         return calculateAge(dobDB) >= 35 ? json({ isAdult: true, photo }) : json({ isAdult: false });
 
     } catch (error: unknown) {
-        logger.error({ error }, "Unexpected server error");
+        logger.error({
+            internalCode: "[FastAPI-SFail]",
+            status: 500,
+            err: error
+        });
         return json({ error: "Invalid ID" }, { status: 400 });
     }
 };
